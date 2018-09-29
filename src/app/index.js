7a9b40f91132fcffca36928a38d1a0ae4613e67f
Wadah Esam <wadah-wleed@hotmail.com>
2018-09-29 15:56:54 +0300

    creating an api for the blockchain to get the blockchain and to add a new block to the current blockchain

---
+++ src/app/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const Blockchain = require('../blockchain/blockchain');
+
+// will run the server on port 3001
+// process.env.HTTP_PORT this will check 
+// if the port was specified in the command
+// line if it is not then it will take the default
+// ( this will allow us to run multiable instances )
+const HTTP_PORT = process.env.HTTP_PORT || 3001;
+
+const app = express();
+const blockchain = new Blockchain();
+
+// applay the bodyParser middleware that 
+// will allow us to recieve json
+app.use(bodyParser.json());
+
+// creating the first route
+app.get('/blocks', (req, res) => {
+  res.json(blockchain.chain);
+})
+
+app.post('/mine', (req, res) => {
+  // the data that will be sent by the user
+  // will be inside req.body.data 
+  const block = blockchain.addBlock(req.body.data);
+  console.log(`New block added: ${block.toString()}`);
+
+  // this will return the blockchain to the user
+  res.redirect('/blocks');
+});
+
+app.listen(HTTP_PORT, () => console.log(`Listening on port ${HTTP_PORT}`));
