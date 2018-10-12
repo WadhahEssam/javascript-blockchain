08deeb8e9cebc583526c95f9e8119b1b0b606e15
Wadah Esam <wadah-wleed@hotmail.com>
2018-10-12 23:24:58 +0300

    moving on with the test of the transaction class

--- test/wallet/transaction.test.js
+++ test/wallet/transaction.test.js
@@ -1,14 +1,26 @@
 const Transaction = require('../../src/wallet/transaction');
 const Wallet = require('../../src/wallet/wallet');
 
+let transaction, wallet, recipient, amount;
+beforeEach(() => {
+  wallet = new Wallet();
+  // amount = 50;
+  // recipient = 'r3c1p13nt';
+  // transaction = Transaction.newTransaction(wallet, recipient, amount);
+});
+
 describe('Transaction', () => {
-  let transaction, wallet, recipient, amount;
-  beforeEach(() => {
-    wallet = new Wallet();
-    amount = 50;
-    recipient = 'r3c1p13nt';
-    transaction = Transaction.
+
+
+  it('creates the first output that includes amount subtracted from the wallet balance', () => {
+    // we will use the find function which 
+    // return the element from the array 
+    // that makes the given condition true
+    // expect(transaction.outputs.find(output => output.address === wallet.publicKey))
+    // .toEqual(wallet.balance - amount);
   });
 
-  it('')
+  // it('creates the second output that has the amount added to the recipient', () => {
+  //   expect(transaction.outputs.find(output => output.address === recipient)).toEqual(amount);
+  // });
 });
